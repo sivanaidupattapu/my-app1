1924d775f16bddf8f1475c9695aa169ad3b93935
sivanaidupattapu <adisivanaidup@gmail.com>
2024-05-06 16:03:01 +0530

    asd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,8 +16,7 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
-
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
