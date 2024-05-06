cb67381124e45617df08c2c28a4150708d015aaf
sivanaidupattapu <adisivanaidup@gmail.com>
2024-05-06 15:01:48 +0530

    basic-routing

---
+++ src/icecream.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+function Ice(){
+    return <div>
+        <h2>butterscotch</h2>
+    </div>
+}
+export default Ice
--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Ice from './icecream';
+import Moterbike from './bike';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      {
+        path: "/ice",
+        element: <Ice></Ice>,
+      },
+      {
+        path: "/bike",
+        element: <Moterbike></Moterbike>,
+      }
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}></RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
