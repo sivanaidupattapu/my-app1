15b4cfd89d3d4a1e2569b729cd8af6834c137179
sivanaidupattapu <adisivanaidup@gmail.com>
2024-05-06 16:04:04 +0530

    asd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,7 +16,7 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <RouterProvider router={router} />
 
 );
 
